ac462e1cc70ed8005cdd7134773338b0b91ed786
elvira egorova <leylaazbuhan@gmail.com>
2025-12-17 04:32:10 +0300

    Initial commit

---
+++ src/main/java/ru/netology/springBootDemo/SpringBootDemoApplication.java
@@ -0,0 +1,51 @@
+package ru.netology.springBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ru.netology.springBootDemo.entity.Person;
+import ru.netology.springBootDemo.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+	@Bean
+		public CommandLineRunner demo(PersonRepository repository) {
+			return args -> {
+				System.out.println("=== Инициализация тестовых данных ===");
+
+				// Создаем тестовые данные
+				Person person1 = new Person("Иван", "Иванов", 30, "+7-999-123-45-67", "Москва");
+				Person person2 = new Person("Петр", "Петров", 25, "+7-999-987-65-43", "Санкт-Петербург");
+				Person person3 = new Person("Анна", "Сидорова", 28, "+7-999-111-22-33", "Москва");
+				Person person4 = new Person("Мария", "Кузнецова", 35, "+7-999-444-55-66", "Казань");
+
+				// Сохраняем в базу
+				repository.save(person1);
+				repository.save(person2);
+				repository.save(person3);
+				repository.save(person4);
+
+				// Выводим все записи
+				System.out.println("Все пользователи в базе:");
+				repository.findAll().forEach(System.out::println);
+
+				// Пример поиска
+				System.out.println("\nПользователи из Москвы:");
+				repository.findByCity("Москва").forEach(System.out::println);
+
+				System.out.println("\nПользователи младше 30 лет:");
+				repository.findByAgeLessThan(30).forEach(System.out::println);
+
+				System.out.println("\nПриложение запущено! Проверьте:");
+				System.out.println("1. H2 Console: http://localhost:8080/h2-console");
+				System.out.println("2. REST API: http://localhost:8080/api/persons");
+			};
+		}
+}
---
+++ src/main/java/ru/netology/springBootDemo/entity/Person.java
@@ -0,0 +1,109 @@
+package ru.netology.springBootDemo.entity;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "PERSONS")
+@IdClass(Person.PersonId.class)
+public class Person {
+
+    @Id
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Id
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Id
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "city")
+    private String city;
+
+    // Конструкторы
+    public Person() {}
+
+    public Person(String name, String surname, int age, String phoneNumber, String city) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+    }
+
+    // Геттеры и сеттеры
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getSurname() { return surname; }
+    public void setSurname(String surname) { this.surname = surname; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', surname='" + surname + "', age=" + age +
+                ", phoneNumber='" + phoneNumber + "', city='" + city + "'}";
+    }
+
+
+    public static class PersonId implements Serializable {
+        private String name;
+        private String surname;
+        private int age;
+
+
+        public PersonId() {}
+
+        public PersonId(String name, String surname, int age) {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+
+        // Геттеры и сеттеры для полей ключа
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+
+        public String getSurname() { return surname; }
+        public void setSurname(String surname) { this.surname = surname; }
+
+        public int getAge() { return age; }
+        public void setAge(int age) { this.age = age; }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            PersonId personId = (PersonId) o;
+            return age == personId.age &&
+                    Objects.equals(name, personId.name) &&
+                    Objects.equals(surname, personId.surname);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, surname, age);
+        }
+
+        @Override
+        public String toString() {
+            return "PersonId{name='" + name + "', surname='" + surname + "', age=" + age + "}";
+        }
+    }
+}
